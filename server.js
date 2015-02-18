b8c74ff034c7ca3f74c232bb40751945cb5531de
will wilco <wgmwilcox@gmail.com>
2015-02-18 14:40:47 -0500

    delete
    ok

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(bodyParser.json({ extended: false }));
 app.use(cors());
 app.use(express.static(__dirname + '/'));
 
-// var url = "http://api.nytimes.com/svc/topstories/v1/home.json?api-key=0c446e47b3a7f6ed45dbc12101701c5a:13:71271148";
-var url = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=cae2e692689c88b2447209095af35d62:12:71271148";
-var url2 = "https://api.forecast.io/forecast/264b9e60bfe658dcdd4582fa7342668d/41,-74";
+// var url = "http://api.nytimes.com/svc/topstories/v1/home.json?api-key=";
+var url = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=";
+var url2 = "https://api.forecast.io/forecast//41,-74";
 
 app.get("/news", function(req, res){
   request(url, function (error, response, body) {
@@ -27,7 +27,7 @@ app.get("/news", function(req, res){
 
 //
 //
-// var url2 = "https://api.forecast.io/forecast/264b9e60bfe658dcdd4582fa7342668d/41,-74";
+// var url2 = "https://api.forecast.io/forecast//41,-74";
 // app.get("/weather", function(req, res){
 //   request(url2, function (error, response, body) {
 //     if (!error && response.statusCode == 200) {
